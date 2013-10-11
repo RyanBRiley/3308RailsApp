828e4ab21e55d302dfa2c0e3d1a5dfc096522097
RyanBRiley <Ryan.B.Riley@colorado.edu>
2013-10-10 22:00:13 -0700

    adding memory capabilities

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,9 +7,45 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = params[:sort] == "title" || params[:sort] == "release_date" ? Movie.order(params[:sort]) : Movie.all
-    @is_active_title = params[:sort] == "title" ? "hilite" : ""
-    @is_active_date = params[:sort] == "release_date" ? "hilite" : ""
+    session_mem = false
+    @all_ratings = Movie.uniq.pluck(:rating).sort
+    @is_active_title = params[:id] == "title_header" ? "hilite" : ""
+    @is_active_date = params[:id] == "release_date" ? "hilite" : ""
+
+    if params[:ratings].blank? && !session[:ratings].blank?
+        params[:ratings] = session[:ratings]
+        session_mem = true
+    end
+    session[:ratings] = params[:ratings]
+
+    if params[:id].blank? && !session[:id].blank?
+        params[:id] = session[:id]
+        session_mem = true
+    end
+    session[:id] = params[:id]
+    
+    if session_mem
+      flash.keep
+      redirect_to movies_path(:id => params[:id], :ratings => params[:ratings])
+    end
+    
+    order_by = params[:id] == "title_header" ? "title" : params[:id] == "release_date" ? "release_date" : "" 
+    @rating_checked = Hash.new(true)
+    @check_box_checked = Hash.new()
+    
+    if !params[:ratings].blank?
+      find_by = params[:ratings].keys.reject {|rating| !@all_ratings.include?(rating)}
+      @all_ratings.each do |rating|
+        @check_box_checked[rating] = 1
+        unless params[:ratings].has_key?(rating)
+          @rating_checked[rating] = false
+          @check_box_checked.delete(rating)
+          end
+        end
+      @movies = Movie.where(rating: find_by).order(order_by)
+      else
+      @movies = Movie.order(order_by)
+      end
   end
 
   def new
